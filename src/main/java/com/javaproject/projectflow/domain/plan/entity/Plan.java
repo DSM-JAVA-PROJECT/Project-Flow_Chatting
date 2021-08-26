652e5c50592a0845f14a6a6d8c76208757a43459
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-26 14:55:48 +0900

    ⚡️ :: Plan Entity 추가

--- src/main/java/com/javaproject/projectflow/domain/plan/entity/Plan.java
+++ src/main/java/com/javaproject/projectflow/domain/plan/entity/Plan.java
@@ -1,7 +1,13 @@
 package com.javaproject.projectflow.domain.plan.entity;
 
+import com.javaproject.projectflow.domain.chatroom.entity.ChatRoom;
+import com.javaproject.projectflow.domain.chatroom.entity.Member;
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -9,4 +15,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Builder
 @Document
 public class Plan {
+
+    @DBRef(lazy = true)
+    private ChatRoom chatRoom;
+
+    private String name;
+
+    private LocalDate endDate;
+
+    private List<Member> members;
+
 }
