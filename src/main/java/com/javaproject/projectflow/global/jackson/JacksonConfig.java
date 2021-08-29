0f9f13dfd2912f907e937b3a8fb4983b27cc1dba
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-29 21:16:59 +0900

    ⚡️ :: JacksonConfig 추가

--- src/main/java/com/javaproject/projectflow/global/jackson/JacksonConfig.java
+++ src/main/java/com/javaproject/projectflow/global/jackson/JacksonConfig.java
@@ -1,2 +1,29 @@
-package com.javaproject.projectflow.global.jackson;public class JacksonConfig {
+package com.javaproject.projectflow.global.jackson;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ResolvableType;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Configuration
+public class JacksonConfig {
+
+    private final Jackson2JsonEncoder jsonEncoder;
+    private final DataBufferFactory dataBufferFactory;
+
+    @Bean
+    Flux<DataBuffer> encodeValue(Class<?> target, Mono<Object> targetObj) {
+        ResolvableType type = ResolvableType.forType(target);
+        if(jsonEncoder.canEncode(type, null)) {
+            return jsonEncoder.encode(targetObj, dataBufferFactory,
+                    ResolvableType.forClass(target), null, null);
+        }
+        return null;
+    }
 }
