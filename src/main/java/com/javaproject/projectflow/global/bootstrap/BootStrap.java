23f0079ac09eff6851c1de7e80fb800378231917
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-29 21:11:47 +0900

    ⚡️ :: CommandLineRunner Queue 선언 추가

---
+++ src/main/java/com/javaproject/projectflow/global/bootstrap/BootStrap.java
@@ -0,0 +1,20 @@
+package com.javaproject.projectflow.global.bootstrap;
+
+import com.javaproject.projectflow.global.rabbit.RabbitMQConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import reactor.rabbitmq.ExchangeSpecification;
+import reactor.rabbitmq.Sender;
+
+@RequiredArgsConstructor
+public class BootStrap implements CommandLineRunner {
+
+    private final Sender sender;
+
+    @Override
+    public void run(String... args) {
+        sender.declareExchange(ExchangeSpecification
+                .exchange(RabbitMQConfig.DIRECT_EXCHANGE)
+                .type("direct"));
+    }
+}
---
+++ src/main/java/com/javaproject/projectflow/global/jackson/JacksonConfig.java
@@ -0,0 +1,2 @@
+package com.javaproject.projectflow.global.jackson;public class JacksonConfig {
+}
