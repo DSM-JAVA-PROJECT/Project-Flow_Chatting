ec2b6830230a501c8ee5ddeef9a96650e2b41505
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-31 19:01:37 +0900

    ⚡️ :: ChatRoomController 추가

--- src/main/java/com/javaproject/projectflow/domain/chatroom/controller/ChatRoomController.java
+++ src/main/java/com/javaproject/projectflow/domain/chatroom/controller/ChatRoomController.java
@@ -1,9 +1,16 @@
 package com.javaproject.projectflow.domain.chatroom.controller;
 
+import com.javaproject.projectflow.domain.chatroom.payload.CreateChatRoomRequest;
+import com.javaproject.projectflow.domain.chatroom.service.ChatRoomService;
+import com.javaproject.projectflow.global.security.CurrentUser;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -11,9 +18,16 @@ import reactor.core.publisher.Mono;
 @RestController
 public class ChatRoomController {
 
+    private final ChatRoomService chatRoomService;
+
+    @MessageMapping("{projectId}.chatroom.add")
+    public Mono<String> createChatRoom(@DestinationVariable @NonNull String projectId, @CurrentUser String email,
+                                       @Payload CreateChatRoomRequest request) {
+        return chatRoomService.createChatRoom(email, request, projectId);
+    }
+
     @MessageExceptionHandler
-    public Mono<ResponseEntity<String>> exception(final Exception exception)
-    {
+    public Mono<ResponseEntity<String>> exception(final Exception exception) {
         return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exception.getMessage()));
     }
