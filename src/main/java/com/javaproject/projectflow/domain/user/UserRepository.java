ec7084df8b224fedf4255e711b80f65f8b750a4e
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-31 16:55:01 +0900

    :recycle: :: Repository 코드 정리

--- src/main/java/com/javaproject/projectflow/domain/user/UserRepository.java
+++ src/main/java/com/javaproject/projectflow/domain/user/UserRepository.java
@@ -1,7 +1,6 @@
 package com.javaproject.projectflow.domain.user;
 
-import com.javaproject.projectflow.domain.chatroom.entity.ChatRoom;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface UserRepository extends ReactiveMongoRepository<ChatRoom, String> {
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
 }
