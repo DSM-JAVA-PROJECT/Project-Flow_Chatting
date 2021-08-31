1b1828fbfb98da16417a8fb6b5fec835813b8db6
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-08-31 16:43:46 +0900

    ⚡️ :: 유저 정보를 가져오는 어노테이션 생성

--- src/main/java/com/javaproject/projectflow/global/security/CurrentUser.java
+++ src/main/java/com/javaproject/projectflow/global/security/CurrentUser.java
@@ -1,2 +1,14 @@
-package com.javaproject.projectflow.global.security;public @interface CurrentUser {
+package com.javaproject.projectflow.global.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@AuthenticationPrincipal(expression = "getClaims().get('sub')")
+public @interface CurrentUser {
 }
