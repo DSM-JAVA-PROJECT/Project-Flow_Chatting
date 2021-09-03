6d67e5e23b9672e7e03fa7fa191b37c704930392
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-03 19:07:06 +0900

    ⚡️ :: start.sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-# BUILD_JAR=$(ls /home/ec2-user/build/*.jar)  # 필요없. 도커 쓸거라서
-# JAR_NAME=$(basename "$BUILD_JAR")           # 필요없
-CONTAINER_NAME="project-flow"
-IMAGE_NAME="project-flow-image"
-# echo "파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+BASE_DIR=/home/ubuntu/
+BUILD_JAR=$($BASE_DIR + "build/*.jar")
+JAR_NAME=$(basename "$BUILD_JAR")
+echo "> build 파일명: $JAR_NAME" >> $BASE_DIR + deploy.log
 
-# echo "파일 복사" >> /home/ubuntu/deploy.log
-# DEPLOY_PATH=/home/ubuntu/                   # 필요없
-# cp "$BUILD_JAR" $DEPLOY_PATH                # 필요없
+echo "> build 파일 복사" >> $BASE_DIR + deploy.log
+DEPLOY_PATH=BASE_DIR
+cp "$BUILD_JAR" $DEPLOY_PATH
 
-# echo "현재 실행중인 Application 확인" >> /home/ubuntu/deploy.log
-sudo docker build -t $IMAGE_NAME /home/ubuntu/build
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> $BASE_DIR/deploy.log
+CURRENT_PID=$(pgrep -f "$JAR_NAME")
 
-if [ -n "$(sudo docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
-  # running이 있다면 stop
-  echo "stopping" >> /home/ubuntu/deploy.log
-  sudo docker stop $CONTAINER_NAME
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $BASE_DIR + deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 "$CURRENT_PID"
+  sleep 5
 fi
-if [ -n "$(sudo docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
-  # 만약 exited 상태의 container가 존재한다면 삭제
-  echo "removing.." >> /home/ubuntu/deploy.log
-  sudo docker rm $CONTAINER_NAME
-fi
-echo "running" >> /home/ubuntu/deploy.log
-# 컨테이너 실행
-sudo docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo "> DEPLOY_JAR 배포"    >> $BASE_DIR + deploy.log
+nohup java -jar "$DEPLOY_JAR" >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
