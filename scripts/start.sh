6da1aca63f154f3732e1e81cb787e23b962269e5
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-03 19:52:20 +0900

    ⚡️ :: test

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,7 +20,8 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> run DEPLOY_JAR"    >> /home/ubuntu/deploy.log
 echo "> MONGO_URL:"
 echo "$MONGO_URL"
-nohup java -jar "$DEPLOY_JAR" >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
+echo "> run DEPLOY_JAR"    >> /home/ubuntu/deploy.log
+
+nohup java -jar "$DEPLOY_JAR"&
