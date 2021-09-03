22dc3fc9f4744aaa60ad198ce7ef78f06d0f8d1a
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-03 15:04:13 +0900

    ⚡️ :: 로그 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,6 +12,7 @@ IMAGE_NAME="project-flow-image"
 # echo "현재 실행중인 Application 확인" >> /home/ubuntu/deploy.log
 sudo docker build --name $IMAGE_NAME /home/ubuntu/build/Dockerfile
 STATUS=!"$(docker ps -a | grep $CONTAINER_NAME)"
+echo "상태: $STATUS" > /home/ubuntu/deploy.log
 
 if $STATUS; then
   if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
@@ -21,6 +22,7 @@ if $STATUS; then
       # 만약 exited 상태의 container가 존재한다면 삭제
       docker rm $CONTAINER_NAME
   fi
+  echo "컨테이너 실행"
   # 컨테이너 실행
   docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
 fi
