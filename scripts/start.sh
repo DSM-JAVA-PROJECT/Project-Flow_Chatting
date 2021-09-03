f2b6cf649263862bb5082b98a32e89c96cd41182
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-03 18:05:41 +0900

    ⚡️ :: Dockerfile 수정 및 docker-compose 삭제

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ IMAGE_NAME="project-flow-image"
 # cp "$BUILD_JAR" $DEPLOY_PATH                # 필요없
 
 # echo "현재 실행중인 Application 확인" >> /home/ubuntu/deploy.log
-# sudo docker build -t $IMAGE_NAME /home/ubuntu/build
+sudo docker build -t $IMAGE_NAME /home/ubuntu/build
 
 if [ -n "$(sudo docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]; then
   # running이 있다면 stop
@@ -24,5 +24,4 @@ if [ -n "$(sudo docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
 fi
 echo "running" >> /home/ubuntu/deploy.log
 # 컨테이너 실행
-ls
-sudo docker-compose up -d --build
+sudo docker run -d --name $CONTAINER_NAME -p 8080:8080 $IMAGE_NAME
