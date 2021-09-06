9b03782e7931fbeb4b2e942ee2978b6d0e44183e
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-06 09:09:14 +0900

    ⚡️ :: UserRepository에 findByEmail 추가

--- src/main/java/com/javaproject/projectflow/domain/user/UserRepository.java
+++ src/main/java/com/javaproject/projectflow/domain/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.javaproject.projectflow.domain.user;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByEmail(String email);
 }
