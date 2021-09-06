512788580f3a3754d947abc4c062273f1f7a704d
Hong Jeong Hyeon <jhhong0509@naver.com>
2021-09-06 09:34:36 +0900

    ⚡️ :: (#5) Dto 추가 및 수정

--- src/main/java/com/javaproject/projectflow/domain/plan/dto/CreatePlanDto.java
+++ src/main/java/com/javaproject/projectflow/domain/plan/dto/CreatePlanDto.java
@@ -2,10 +2,12 @@ package com.javaproject.projectflow.domain.plan.dto;
 
 import com.javaproject.projectflow.domain.plan.payload.PlanRequest;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class CreatePlanDto {
 
     private final PlanRequest request;
